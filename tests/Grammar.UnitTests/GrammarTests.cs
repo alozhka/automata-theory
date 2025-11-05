d698ea96d2816426864084cf122ca73bfc42d503
Sergey Lozhkin <alozhka@yahoo.com>
2025-11-05 20:13:23 +0300

    tests: расписал тесты для проверки грамматики

--- tests/Grammar.UnitTests/GrammarTests.cs
+++ tests/Grammar.UnitTests/GrammarTests.cs
@@ -1,9 +1,38 @@
+using Antlr4.Runtime.Misc;
+
 namespace Grammar.UnitTests;
 
 public class GrammarTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [MemberData(nameof(ListValidStatements))]
+    public void Accepts_valid_statements(string stmt)
     {
+        MysticGameScript.ValidateQuery(stmt);
+    }
+
+    [Theory]
+    [MemberData(nameof(ListInvalidStatements))]
+    public void Rejects_invalid_statements(string stmt)
+    {
+        Assert.Throws<ParseCanceledException>(() => MysticGameScript.ValidateQuery(stmt));
+    }
+
+    public static TheoryData<string> ListValidStatements()
+    {
+        return new TheoryData<string>
+        {
+            // целочисленная переменная
+            "dayzint x;",
+        };
+    }
+
+    public static TheoryData<string> ListInvalidStatements()
+    {
+        return new TheoryData<string>
+        {
+            // Отсутствует точка с запятой
+            "dayzint x",
+        };
     }
 }
